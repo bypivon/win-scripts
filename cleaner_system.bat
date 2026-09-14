883e411a891562411790b1ede78206a3dee6c3d3
bypivon <bypivon@protonmail.com>
2026-09-14 02:01:50 -0300

    save files with encoding

--- cleaner_system.bat
+++ cleaner_system.bat
@@ -1,5 +1,5 @@
+chcp 65001 >nul 2>&1
 @echo off
-REM chcp 65001 >nul 2>&1
 title bypivon_system_cleaner_script - bypivon@protonmail.com
 REM =============================================
 REM script para Windows 10/11
